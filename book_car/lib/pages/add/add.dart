2b7ae470922af223d6982c75e1b4b88974fbd21e
huoxiaoye <17711034@bjtu.edu.cn>
2019-10-07 17:51:59 +0800

    底部导航栏和页面切换完毕

---
+++ book_car/lib/pages/add/add.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class Add extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text("增加页面")
+    );
+  }
+}
---
+++ book_car/lib/pages/person/person.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class Person extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text("个人页面")
+    );
+  }
+}
